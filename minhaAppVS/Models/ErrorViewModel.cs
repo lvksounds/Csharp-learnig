ef9487033d3fcf330545b8c7100dd426452ef038
lucas.freitas <lvksounds@gmail.com>
2022-05-10 08:19:10 -0300

    Repositorio de estudos em C#

---
+++ minhaAppVS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace minhaAppVS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
