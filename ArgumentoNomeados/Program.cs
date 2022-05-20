56c606962ee44aed56a3213d8bacfbb0af23976d
lucas.freitas <lvksounds@gmail.com>
2022-05-20 16:20:08 -0300

    att 20-05

---
+++ ArgumentoNomeados/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace ParametroOpcionais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int parametro01;
+            string parametro02;
+
+
+            Console.WriteLine("Digite o parametro para o numero 02: (texto): ");
+            parametro02 = Console.ReadLine();
+            Console.WriteLine("Agora digite o parametro opcional (texto): ");
+            parametro01 = int.Parse(Console.ReadLine());
+
+            // chamando o paramtro nomeado
+            paramOpcional(par2: parametro02, par1: parametro01);
+
+            Console.WriteLine();
+        }
+        static void paramOpcional(int par1, string par2 = "Inserido pelo metodo")
+        {
+            string resultado = "Parametro obrigatorio: " + par1.ToString() + " Parametro opcional: " + par2;
+            Console.WriteLine(resultado);
+        }
+    }
+}
