e723458df57628f079c9a7665fdee10a06d990a7
lucas.freitas <lvksounds@gmail.com>
2022-05-26 10:33:31 -0300

    att 26/05

---
+++ Exercitando/CalcProj1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System; 
+
+namespace CalcProj1
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num1, num2;
+            string choose = "";
+
+            while(choose != "0")
+            {
+                Console.WriteLine("Calculadora de dois digitos");
+                Console.WriteLine("Digite o número da operação a ser executada.");
+                Console.WriteLine("1. Adição - 2. Subtração - 3. Multiplicação - 4. Divisão. 5. Potênciação (1° valor ^ 2° valor) - 0. Encerrar.");
+                choose = Console.ReadLine();
+
+                Console.WriteLine("1° valor: ");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("2° valor: ");
+                num2 = int.Parse(Console.ReadLine());
+
+                
+
+                if (choose == "1")
+                {
+                    Console.WriteLine("Resultado: " + calcs.Soma(num1, num2));
+                }
+                else if (choose == "2")
+                {
+                    Console.WriteLine("Resultado: " + calcs.Sub(num1, num2));
+                }
+                else if (choose == "3")
+                {
+                    Console.WriteLine("Resultado: " + calcs.Multi(num1, num2));
+                }
+                else if (choose == "4")
+                {
+                    Console.WriteLine("Resultado: " + calcs.Div(Convert.ToDouble(num1), Convert.ToDouble(num2)));
+                }
+                else if (choose == "5")
+                {
+                    Console.WriteLine("Resultado: " + calcs.Pot(Convert.ToDouble(num1), Convert.ToDouble(num2)));    
+                } 
+
+                else if (choose == "0")
+                {
+                    Console.WriteLine("Encerrando Calculadora");
+                    break;
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ Exercitando/CalcProj1/calcs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcProj1
+{
+    public class calcs
+    {
+        public static int Soma(int num1, int num2)
+        {
+            int soma = num1 + num2;
+            return soma;
+        }
+
+        public static double Div(double num1, double num2)
+        {
+            double div = num1 / num2;
+            return div;
+        }
+
+        public static long Multi(int num1, int num2)
+        {
+            
+            return (Math.BigMul(num1, num2));
+        }
+
+        public static double Sub(double num1, double num2)
+        {
+            double sub = num1 - num2;
+            return sub;
+        }
+        public static double Pot(double num1, double num2)
+        {
+            return Math.Pow(num1, num2);
+        }
+    }
+}
